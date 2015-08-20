d5ef9266b1cb92414af6d602312b64995b9959a9
LinQuan <linquan@hexindai.com>
2015-08-20 09:43:03 +0800

    third commit

--- script/localStorageTpl.js
+++ script/localStorageTpl.js
@@ -1,71 +1,77 @@
-$.lsTplList = {};
-$.lsTplPending = {};
-
-$.lsTpl = function(name, url) {
-    var _this = this;
-    var deferred = $.Deferred();
-    var tpl;
-    if(!name || !url) {
-        deferred.reject();
+(function( factory ) {
+    if ( typeof define === "function" && define.amd ) {
+        define( ["jquery"], factory );
+    } else {
+        factory( jQuery );
     }
-    name = 'lsT_' + name;
-    if($.lsTplList[name]) {
-        tpl = $.lsTplList[name];
-    }
-    if(!tpl && _this.support && localStorage.getItem(name)) {
-        var tpl_tmp;
-        try {
-            tpl_tmp = JSON.parse(localStorage.getItem(name));
-            if(url == tpl_tmp.url) {
-                tpl = tpl_tmp.source;
-                $.lsTplList[name] = tpl;
-            }
-        } catch(e) {
+} (function($) {
+    $.lsTplList = {};
+    $.lsTplPending = {};
 
+    $.lsTpl = function(name, url) {
+        var support = !!window.localStorage;
+        var deferred = $.Deferred();
+        var tpl;
+        if(!name || !url) {
+            deferred.reject();
         }
-    }
-
-    if(tpl) {
-        deferred.resolve(tpl);
-    }
-
-    if(!tpl && $.lsTplPending[name]) {
-        $.lsTplPending[name].push(deferred);
-    }
-
-    if(!tpl && !$.lsTplPending[name]) {
-        $.lsTplPending[name] = [];
-        $.lsTplPending[name].push(deferred);
-        $.ajax({
-            url: url,
-            dataType: 'html',
-            success: function(html) {
-                if(html) {
-                    tpl = html;
-                    if(_this.support) {
-                        var data_tmp = {
-                            url: url,
-                            source: tpl
-                        };
-                        localStorage.setItem(name, JSON.stringify(data_tmp));
-                    }
+        name = 'lsT_' + name;
+        if($.lsTplList[name]) {
+            tpl = $.lsTplList[name];
+        }
+        if(!tpl && support && localStorage.getItem(name)) {
+            var tpl_tmp;
+            try {
+                tpl_tmp = JSON.parse(localStorage.getItem(name));
+                if(url == tpl_tmp.url) {
+                    tpl = tpl_tmp.source;
                     $.lsTplList[name] = tpl;
-                    for(var i=0;i<$.lsTplPending[name].length;i++) {
-                        $.lsTplPending[name][i].resolve(tpl);
-                    }
-                    delete $.lsTplPending[name];
                 }
-            },
-            error: function() {
-                for(var i=0;i<$.lsTplPending[name].length;i++) {
-                    $.lsTplPending[name][i].reject();
-                }
+            } catch(e) {
+
             }
-        });
-    }
-    return deferred.promise();
-};
+        }
 
-$.lsTpl.prototype.support = !!window.localStorage;
+        if(tpl) {
+            deferred.resolve(tpl);
+        }
 
-$.localStorageTpl = $.lsTpl;
+        if(!tpl && $.lsTplPending[name]) {
+            $.lsTplPending[name].push(deferred);
+        }
+
+        if(!tpl && !$.lsTplPending[name]) {
+            $.lsTplPending[name] = [];
+            $.lsTplPending[name].push(deferred);
+            $.ajax({
+                url: url,
+                dataType: 'html',
+                success: function(html) {
+                    if(html) {
+                        tpl = html;
+                        if(support) {
+                            var data_tmp = {
+                                url: url,
+                                source: tpl
+                            };
+                            localStorage.setItem(name, JSON.stringify(data_tmp));
+                        }
+                        $.lsTplList[name] = tpl;
+                        for(var i=0;i<$.lsTplPending[name].length;i++) {
+                            $.lsTplPending[name][i].resolve(tpl);
+                        }
+                        delete $.lsTplPending[name];
+                    }
+                },
+                error: function() {
+                    for(var i=0;i<$.lsTplPending[name].length;i++) {
+                        $.lsTplPending[name][i].reject();
+                    }
+                }
+            });
+        }
+        return deferred.promise();
+    };
+
+    $.localStorageTpl = $.lsTpl;
+})(jQuery);
